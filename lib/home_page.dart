9c0448b3194970da275ce888f90562fecc9aa8df
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2024-03-07 13:07:45 +0800

    speechToText method added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:allen/feature_box.dart';
 import 'package:allen/pallete.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future <void> initSpeechToText() async{
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  Future <void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future <void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result){
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,102 +54,112 @@ class _HomePageState extends State<HomePage> {
         leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          //Virtual Assistant Picture
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
-                  decoration: const BoxDecoration(
-                    color: Pallete.assistantCircleColor,
-                    shape: BoxShape.circle,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //Virtual Assistant Picture
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle,
+                    ),
                   ),
                 ),
-              ),
-              Center(
-                child: Container(
-                  height: 123,
-                  width: 123,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(image: AssetImage('assets/images/virtualAssistant.png'))
+                Center(
+                  child: Container(
+                    height: 123,
+                    width: 123,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(image: AssetImage('assets/images/virtualAssistant.png'))
+                    ),
                   ),
-                ),
-              )
-            ],
-          ),
-          //Chat Bubble
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10
+                )
+              ],
             ),
-            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
-              top: 30,
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Pallete.borderColor
+            //Chat Bubble
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10
               ),
-              borderRadius: BorderRadius.circular(20).copyWith(
-                topLeft: Radius.zero,
-              )
+              margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+                top: 30,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor
+                ),
+                borderRadius: BorderRadius.circular(20).copyWith(
+                  topLeft: Radius.zero,
+                )
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text("Good morning, what task can I do for you?",
+                style: TextStyle(
+                  color: Pallete.mainFontColor,
+                  fontSize: 25,
+                  fontFamily: 'Cera Pro'
+                ),
+                ),
+              ),
             ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: Text("Good morning, what task can I do for you?",
-              style: TextStyle(
+            Container(
+              padding: const EdgeInsets.all(10),
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(
+                top: 10,
+                left: 22,
+                ),
+              child: const Text("Here are a few features",style: TextStyle(
+                fontFamily: 'Cera Pro',
                 color: Pallete.mainFontColor,
-                fontSize: 25,
-                fontFamily: 'Cera Pro'
-              ),
-              ),
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.only(
-              top: 10,
-              left: 22,
-              ),
-            child: const Text("Here are a few features",style: TextStyle(
-              fontFamily: 'Cera Pro',
-              color: Pallete.mainFontColor,
-              fontSize: 20,
-              fontWeight: FontWeight.bold
-            ),),
-          ),
-          //Features List
-          const Column(
-            children: [
-              FeatureBox(
-                color: Pallete.firstSuggestionBoxColor,
-                headerText: "ChatGPT",
-                descriptionText: "A smarter way to stay organized and informed with ChatGPT",
-              ),
-              FeatureBox(
-                color: Pallete.secondSuggestionBoxColor,
-                headerText: "Dall-E",
-                descriptionText: "Get inspired and stay creative with your personal assistant powered by Dall-E",
-              ),
-              FeatureBox(
-                color: Pallete.thirdSuggestionBoxColor,
-                headerText: "Smart Voice Assistant",
-                descriptionText: "Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT",
-              )
-            ],
-          )
-
-        ],
+            //Features List
+            const Column(
+              children: [
+                FeatureBox(
+                  color: Pallete.firstSuggestionBoxColor,
+                  headerText: "ChatGPT",
+                  descriptionText: "A smarter way to stay organized and informed with ChatGPT",
+                ),
+                FeatureBox(
+                  color: Pallete.secondSuggestionBoxColor,
+                  headerText: "Dall-E",
+                  descriptionText: "Get inspired and stay creative with your personal assistant powered by Dall-E",
+                ),
+                FeatureBox(
+                  color: Pallete.thirdSuggestionBoxColor,
+                  headerText: "Smart Voice Assistant",
+                  descriptionText: "Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT",
+                )
+              ],
+            )
+        
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Pallete.firstSuggestionBoxColor,
-        onPressed: (){},
+        onPressed: ()async{
+          if(await speechToText.hasPermission && speechToText.isNotListening){
+            await startListening();
+          }else if(speechToText.isListening){
+            await stopListening();
+          }else{
+            initSpeechToText();
+          }
+        },
         child: const Icon(Icons.mic),
       ),
     );
